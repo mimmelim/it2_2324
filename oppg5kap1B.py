0c6ae3a20833187712f67010ba84ae28c9e9c7ba
mimmelim <margareth.rimmereide@vtfk.no>
2023-09-19 15:45:33 +0200

    Update oppg5kap1B.py

--- oppg5kap1B.py
+++ oppg5kap1B.py
@@ -1,2 +1,8 @@
-tall = 5
-print(tall)
+# temperatur-kalkulator 
+
+# input fra bruker --> temperatur og temperaturtype (F eller C)
+# hente ut siste tegnet i input --> dersom F eller dersom C ----- (valgsetning if...)
+# hente ut talla i teperatur-input og regne om
+# regne om mellom F og C -- i riktig retning
+# skrive ut temp med riktig benevnelse
+
